13d40fe0b9e6981edd9d16483a28d528bbfe0391
francis omondi <francisomondi17@gmail.com>
2024-01-10 17:44:33 +0300

    initial commit

---
+++ Components/LoginForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const LoginForm= (props: Props) => {
+  return (
+    <div className='grid place-items-center h-screen'>
+        <div>
+            <h1>Enter your Login Details</h1>
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm
