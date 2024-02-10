0cc1b4d97903ffea7d095b48aa07520ef392ca80
francis omondi <francisomondi17@gmail.com>
2024-02-10 07:39:36 +0300

    user reg functionality

--- Components/RegisterForm.tsx
+++ Components/RegisterForm.tsx
@@ -1,19 +1,39 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
 const RegisterForm = (props: Props) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    if(!name || !email || !password){
+      setError('All fields are required')
+      return
+    }
+
+  }
+
+
+
   return (
     <div className='grid place-items-center h-screen'>
         <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400'>
             <h1 className='text-xl font-bold my-4'>Register an account</h1>
-            <form className='flex flex-col gap-3'>
-              <input type='text' placeholder='Full Name'/>
-              <input type='email' placeholder='Email'/>
-              <input type='password' placeholder='Password'/>
+            <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+              <input onChange={(e)=>setName(e.target.value)} type='text' placeholder='Full Name'/>
+              <input onChange={(e)=>setEmail(e.target.value)} type='email' placeholder='Email'/>
+              <input onChange={(e)=>setPassword(e.target.value)} type='password' placeholder='Password'/>
               <button className='bg-green-600 text-white font-bold cursor-pointer px-6 py-2'>Register</button>
-              <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>Error message</div>
+              { error && (
+                <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>{error}</div>
+                )
+              }
               <Link className='text-sm mt-3 text-right'
               href={'/login'}>
                 Already registered? <span className='underline'>Login</span>
