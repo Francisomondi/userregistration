6afc7f8ffdd9d83281f764fe0d32287f527e4e08
francis omondi <francisomondi17@gmail.com>
2024-02-10 06:48:44 +0300

    dashboard

--- Components/RegisterForm.tsx
+++ Components/RegisterForm.tsx
@@ -10,7 +10,7 @@ const RegisterForm = (props: Props) => {
             <h1 className='text-xl font-bold my-4'>Register an account</h1>
             <form className='flex flex-col gap-3'>
               <input type='text' placeholder='Full Name'/>
-              <input type='text' placeholder='Email'/>
+              <input type='email' placeholder='Email'/>
               <input type='password' placeholder='Password'/>
               <button className='bg-green-600 text-white font-bold cursor-pointer px-6 py-2'>Register</button>
               <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>Error message</div>
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import UserInfo from '@/Components/UserInfo'
+import React from 'react'
+
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <UserInfo/>
+  )
+}
+
+export default Dashboard
